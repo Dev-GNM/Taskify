c75837add3ac8960740befe267c7f762445551e5
Geoffrey Njihia <geoffreynjihia15@gmail.com>
2023-05-11 02:55:56 -0700

    added edit functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,8 @@ import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from "react-icons/md";
 import "./styles.css";
+import TodoList from './TodoList';
+import { useState } from "react";
 
 type Props = {
     todo: Todo;
@@ -12,25 +14,44 @@ type Props = {
 
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+
+    const [edit, setEdit ] = useState<boolean>(false);
+    const [editTodo, setEditTodo ] = useState<string>(todo.todo);
+ 
+const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => 
+    todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+    )
+    );
+};
+
+
+const handleDelete = (id: number) => {
+    setTodos(todos.filter((todos) => todo.id !== id));
+    
+};
+
   return (
     <form className='todos_single'>
-        <span className="todos_single--text">
-            {todo.todo}
+        {
+            todo.isDone ? (
+                <s className="todos_single--text">{todo.todo}</s>
+            ): (
+                <span className="todos_single--text">{todo.todo}</span>
+            )}
+      
+
             <div>
                 <span className="icon">
                     <AiFillEdit />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => handleDelete(todo.id)}>
                     <AiFillDelete />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => handleDone(todo.id)}>
                     <MdDone />
                 </span>
             </div>
-
-        </span>
-
-
     </form>
   )
 }
