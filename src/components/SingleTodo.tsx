f2bbaeae599cd1b8a9624c2104f7c5b58829e212
Geoffrey Njihia <geoffreynjihia15@gmail.com>
2023-05-11 04:13:56 -0700

    added delete functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from "react-icons/md";
 import "./styles.css";
 import TodoList from './TodoList';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type Props = {
     todo: Todo;
@@ -31,10 +31,25 @@ const handleDelete = (id: number) => {
     
 };
 
+const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(todos.map((todo) => (
+        todo.id === id? {...todo, todo:editTodo}: todo ))
+        );
+        setEdit(false);
+};
+const inputRef = useRef<HTMLInputElement>(null);
+
+useEffect(() => {
+  inputRef.current?.focus();
+}, [edit]);
+
+
   return (
-    <form className='todos_single'>
+    <form className='todos_single' onSubmit={(e) => handleEdit(e, todo.id)}>
         { edit ? (
             <input 
+            ref={inputRef}
             value={editTodo} 
             onChange={(e) => setEditTodo(e.target.value)} 
             className='todos_single--text'
