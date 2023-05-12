d39a74947c6a8766a8a53b346ace830310e87b1b
Geoffrey Njihia <geoffreynjihia15@gmail.com>
2023-05-12 02:40:40 -0700

    added a state for completed tasks

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import TodoList from './components/TodoList';
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setcompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -28,7 +29,7 @@ const App: React.FC = () => {
     <div className="App">
     <span className="heading">Taskify</span>
     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-    <TodoList todos={todos} setTodos={setTodos}/>
+    <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos} setCompletedTodos={setcompletedTodos}/>
     </div>
   );
 }
